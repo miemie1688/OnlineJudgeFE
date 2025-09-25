bc0aa5a9c817d41b2c103120d8d5e9f574c0d7af
miemie1688 <42808093+miemie1688@users.noreply.github.com>
2025-09-25 15:58:20 +0800

    更新网站标题和链接

--- src/pages/oj/App.vue
+++ src/pages/oj/App.vue
@@ -7,7 +7,7 @@
       </transition>
       <div class="footer">
         <p v-html="website.website_footer"></p>
-        <p>Powered by <a href="https://github.com/QingdaoU/OnlineJudge">AIKeSi_OnlineJudge</a>
+        <p>Powered by <a href="https://github.com/miemie1688/OnlineJudge">AIKeSi</a>
           <span v-if="version">&nbsp; Version: {{ version }}</span>
         </p>
       </div>
